ab1782340b96baf1426a058edaec7a9654a879d4
pivaldi <git@ivaldi.me>
2026-04-24 17:14:21 +0200

    refactor: simplify isLoopSource

--- cmd/mmw/cmd/check/arch.go
+++ cmd/mmw/cmd/check/arch.go
@@ -14,14 +14,11 @@ func NewArchCmd() *cobra.Command {
 		Short: "Validate architectural boundaries",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			root := platform.RootRepo()
-			exitCode := archtest.RunAll(root)
-			err := error(nil)
-
-			if exitCode != 0 {
-				err = errors.New("arch test failed")
+			if archtest.RunAll(root) != 0 {
+				return errors.New("arch test failed")
 			}
 
-			return err
+			return nil
 		},
 	}
 }
